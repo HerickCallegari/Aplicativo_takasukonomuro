70664ac2e20586f512855fd3bc590e4757f3facc
Herick Campos <herickzeno@gmail.com>
2024-10-31 15:12:42 -0300

    Mensagem de erro caso haja algum campo incompleto no formulario e caso o usuario erre a senha

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -12,18 +12,20 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String nome = '';
+  String senha = '';
+  String? errorMessage;
+
+  List<Funcionario> funcionarios = [
+    Funcionario(nome: "herick", senha: "123", cargo: Cargo.Garcom),
+    Funcionario(nome: "Kiria", senha: "123", cargo: Cargo.Gerente),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    String nome = '';
-    String senha = '';
-
-    List<Funcionario> funcionarios = [
-      Funcionario(nome: "herick", senha: "123", cargo: Cargo.Garcom),
-      Funcionario(nome: "Kiria", senha: "123", cargo: Cargo.Gerente),
-    ];
     return Scaffold(
-      resizeToAvoidBottomInset:
-          false, // Impede que o layout seja redimensionado
+      resizeToAvoidBottomInset: false,
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           final width = constraints.maxWidth;
@@ -39,16 +41,16 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               Positioned(
-                top: 40, // Posiciona a seta um pouco abaixo do topo
-                left: 20, // Posiciona a seta um pouco à direita da borda
+                top: 40,
+                left: 20,
                 child: GestureDetector(
                   onTap: () {
-                    Navigator.pop(context); // Volta para a tela anterior
+                    Navigator.pop(context);
                   },
                   child: const Icon(
                     Icons.arrow_back,
                     size: 30,
-                    color: Colors.black, // Cor da seta
+                    color: Colors.black,
                   ),
                 ),
               ),
@@ -56,13 +58,12 @@ class _LoginPageState extends State<LoginPage> {
                 child: Container(
                   decoration: BoxDecoration(
                     color: const Color.fromARGB(255, 248, 248, 248),
-                    borderRadius:
-                        BorderRadius.circular(20), // Arredonda as bordas
+                    borderRadius: BorderRadius.circular(20),
                     boxShadow: const [
                       BoxShadow(
                         color: Colors.black26,
-                        blurRadius: 10, // Suaviza o contorno da sombra
-                        offset: Offset(0, 4), // Posição da sombra
+                        blurRadius: 10,
+                        offset: Offset(0, 4),
                       ),
                     ],
                   ),
@@ -70,145 +71,145 @@ class _LoginPageState extends State<LoginPage> {
                   width: width * 0.72,
                   child: Padding(
                     padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const Text(
-                          'login',
-                          style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.w700,
-                              fontFamily: 'PlayFairDisplay'),
-                          textAlign: TextAlign.left,
-                        ),
-                        const SizedBox(height: 10),
-                        const Text(
-                          'por favor, insira nome e senha',
-                          style: TextStyle(
-                              fontSize: 14, fontFamily: 'PlayFairDisplay'),
-                        ),
-                        const SizedBox(height: 20),
-                        const Text(
-                          'Nome',
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'PlayFairDisplay'),
-                        ),
-                        const SizedBox(height: 5),
-                        TextField(
-                          onChanged: (value) => nome = value,
-                          style: const TextStyle(
-                              fontFamily: 'Roboto',
-                              fontSize: 16), // Fonte Roboto
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            hintText: 'Ex: Herick Campos Calligari',
-                            hintStyle: const TextStyle(
-                                fontFamily: 'Roboto'), // Fonte Roboto no hint
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          const Text(
+                            'login',
+                            style: TextStyle(
+                                fontSize: 32,
+                                fontWeight: FontWeight.w700,
+                                fontFamily: 'PlayFairDisplay'),
+                            textAlign: TextAlign.left,
                           ),
-                        ),
-                        const SizedBox(height: 20),
-                        const Text(
-                          'Senha',
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'PlayFairDisplay'),
-                        ),
-                        const SizedBox(height: 5),
-                        TextField(
-                          onChanged: (value) => senha = value,
-                          obscureText: true, // Oculta a senha
-                          style: const TextStyle(
-                              fontFamily: 'Roboto',
-                              fontSize: 16), // Fonte Roboto
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            hintText: 'Ex: 000000000',
-                            hintStyle: const TextStyle(
-                                fontFamily: 'Roboto'), // Fonte Roboto no hint
+                          const SizedBox(height: 10),
+                          const Text(
+                            'por favor, insira nome e senha',
+                            style: TextStyle(
+                                fontSize: 14, fontFamily: 'PlayFairDisplay'),
                           ),
-                        ),
-                        const SizedBox(height: 20),
-                        /*const Text(
-                          'Cargo',
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'PlayFairDisplay'),
-                        ),
-                        const SizedBox(height: 5),
-                        DropdownButtonFormField<String>(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            hintText: 'Ex: Gerente',
-                            hintStyle: const TextStyle(
-                                fontFamily: 'Roboto'), // Fonte Roboto no hint
+                          const SizedBox(height: 20),
+                          const Text(
+                            'Nome',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'PlayFairDisplay'),
                           ),
-                          style: const TextStyle(
-                              fontFamily: 'Roboto'), // Fonte Roboto no dropdown
-                          items: const [
-                            DropdownMenuItem(
-                                value: 'Gerente', child: Text('Gerente')),
-                            DropdownMenuItem(
-                                value: 'Funcionário',
-                                child: Text('Funcionário')),
-                          ],
-                          onChanged: (value) {},
-                        ),*/
-                        const SizedBox(height: 30),
-                        ElevatedButton(
-                          onPressed: () {
-                            print(nome);
-                            print(senha);
+                          const SizedBox(height: 5),
+                          TextFormField(
+                            onChanged: (value) => nome = value,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Por favor, insira o nome';
+                              }
+                              return null;
+                            },
+                            style: const TextStyle(
+                                fontFamily: 'Roboto', fontSize: 16),
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              hintText: 'Ex: Herick Campos Calligari',
+                              hintStyle: const TextStyle(fontFamily: 'Roboto'),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          const Text(
+                            'Senha',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'PlayFairDisplay'),
+                          ),
+                          const SizedBox(height: 5),
+                          TextFormField(
+                            onChanged: (value) => senha = value,
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Por favor, insira a senha';
+                              }
+                              return null;
+                            },
+                            style: const TextStyle(
+                                fontFamily: 'Roboto', fontSize: 16),
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              hintText: 'Ex: 000000000',
+                              hintStyle: const TextStyle(fontFamily: 'Roboto'),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          if (errorMessage != null)
+                            Text(
+                              errorMessage!,
+                              style: const TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 14,
+                                  fontFamily: 'Roboto'),
+                            ),
+                          const SizedBox(height: 30),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                Funcionario? funcionario =
+                                    funcionarios.firstWhere(
+                                        (Funcionario func) =>
+                                            func.GetNome() == nome,
+                                        orElse: null);
 
-                            Funcionario? funcionario = funcionarios.firstWhere(
-                                (Funcionario func) => func.GetNome() == nome);
-                            if (funcionario == null)
-                              print("este funcionario nao existe");
+                                if (funcionario == null) {
+                                  setState(() {
+                                    errorMessage = "Funcionário não encontrado";
+                                  });
+                                  return;
+                                }
 
-                            if (funcionario.senha != senha)
-                              print("senha incorreta.");
-                            if (funcionario.senha != senha ||
-                                funcionario.nome != nome) {
-                              print("nao foi possivel fazer o login");
-                              return;
-                            } else if (funcionario.cargo == Cargo.Gerente) {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => GerentePage()));
-                            } else if (funcionario.cargo == Cargo.Garcom) {
-                              print(funcionario.nome);
-                              print(funcionario.senha);
-                              print(funcionario.cargo);
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => MesasPage()));
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(vertical: 15),
-                            backgroundColor: Colors.black,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
+                                if (funcionario.senha != senha) {
+                                  setState(() {
+                                    errorMessage = "Senha incorreta";
+                                  });
+                                  return;
+                                }
+
+                                setState(() {
+                                  errorMessage = null;
+                                });
+
+                                if (funcionario.cargo == Cargo.Gerente) {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => GerentePage()));
+                                } else if (funcionario.cargo == Cargo.Garcom) {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => MesasPage()));
+                                }
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(vertical: 15),
+                              backgroundColor: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: const Text(
+                              'LOG IN',
+                              style: TextStyle(color: Colors.white),
                             ),
                           ),
-                          child: const Text(
-                            'LOG IN',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
